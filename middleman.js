24a9adc61c5235c7268ac04a1ae0f89b08b0c7de
Jonathan Hensley <jhensley@gannett.com>
2016-04-15 14:02:04 -0400

    missed closing quote

--- middleman.js
+++ middleman.js
@@ -1,6 +1,6 @@
 // Simulate config options from your production environment by
 // customising the .env file in your project's root folder.
-if (process.env.NODE_ENV !== 'production) {
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').load();
 }
 
